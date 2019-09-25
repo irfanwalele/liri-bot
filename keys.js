023197d6e6313d623007661325b6f75a0b0828de
irfanwalele <irfanwalele@gmail.com>
2019-09-25 02:10:52 -0400

    updated

---
+++ keys.js
@@ -0,0 +1,20 @@
+
+// console.log('loading keys & IDs...');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.bandsintown = {
+  app_id: process.env.BANDSINTOWN_ID
+};
+
+exports.omdb = {
+  apikey: process.env.OMDB_KEY
+};
+
+console.log('this is loaded');
+
+
+                                                                             
